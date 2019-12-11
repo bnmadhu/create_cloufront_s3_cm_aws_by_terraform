b5549bb23054995c84bbd0c525e4fa6d026c0c2b
joelgaria <joel.garia93@gmail.com>
2019-12-11 11:57:41 +0100

    Create var_input.tf

---
+++ var_input.tf
@@ -0,0 +1,31 @@
+variable "Region" {
+  default = "eu-west-1"
+}
+variable "Name" {
+  default = "test-joel-gucci.com"
+}
+variable "Project" {
+  default = "test8-joel-gucci.com"
+}
+variable "Env" {
+  default = "Prod"
+}
+variable "TeamName" {
+  default = "Cid"
+}
+variable "Application" {
+  default = "Cid"
+}
+variable "Owner" {
+  default = "micael.barilaro@gucci.com"
+}
+
+variable "Origin" {
+  default = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E3L7RC9QS5XPDZ"
+}
+variable "origin_access_identity" {
+  default = "origin-access-identity/cloudfront/E3L7RC9QS5XPDZ"
+}
+
+
+
