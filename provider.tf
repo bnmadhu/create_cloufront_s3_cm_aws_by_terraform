353ac99e481273b415557238a48d5c257c606ab6
joelgaria <joel.garia93@gmail.com>
2019-12-11 12:10:35 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  profile    = "joelDevStorm"
+  profile    = "YOUR-PROFILE"
   region     = var.Region
   assume_role {
-    role_arn     = "arn:aws:iam::596649013084:role/sdk-joel-test"
+    role_arn     = "YOUR-ARN-ASSUME-ROLE"
   }
 }
 
 provider "aws" {
-  profile    = "joelDevStorm"
+  profile    = "YOUR-PROFILE"
   region     = "us-east-1"
   assume_role {
-    role_arn     = "arn:aws:iam::596649013084:role/sdk-joel-test"
+    role_arn     = "YOUR-ARN-ASSUME-ROLE"
   }
   alias = "virginia"
 }
